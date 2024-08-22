6391a03d9c60cea160809428dd7f2727a152fcbe
ModestRaf <tanya.stargate@yandex.ru>
2024-08-22 15:28:16 +0300

    работа над api

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import {initialCards} from './components/cards.js';
 import {createCard, deleteCard} from './components/card.js';
 import {openPopup, closePopup} from './components/modal.js';
 import {enableValidation, clearValidation} from './scripts/validation.js';
-import {getUserInfo} from './scripts/api.js';
+import {getUserInfo, getInitialCards} from './scripts/api.js';
 import avatar from './images/avatar.jpg';
+
+//аватарка
+const profileImage = document.querySelector('.profile__image');
+profileImage.style.backgroundImage = `url(${avatar})`;
 
 // Настройки для валидации
 const validationConfig = {
@@ -23,8 +27,6 @@ const buttonsClosePopup = document.querySelectorAll('.popup__close');
 //профиль
 const buttonEditProfile = document.querySelector('.profile__edit-button');
 const buttonAddCard = document.querySelector('.profile__add-button');
-const profileTitle = document.querySelector('.profile__title');
-const profileDescription = document.querySelector('.profile__description');
 //редактирование профиля
 const formEditProfile = document.querySelector('form[name="edit-profile"]');
 const nameInput = formEditProfile.querySelector('input[name="name"]');
@@ -33,16 +35,9 @@ const jobInput = formEditProfile.querySelector('input[name="description"]');
 const formNewPlace = document.querySelector('form[name="new-place"]');
 const placeNameInput = formNewPlace.querySelector('input[name="place-name"]');
 const placeLinkInput = formNewPlace.querySelector('input[name="link"]');
-//аватарка
-const profileImage = document.querySelector('.profile__image');
-profileImage.style.backgroundImage = `url(${avatar})`;
 
 // Контейнер для карточек и вывод карточек на страницу через places__list и перебор массива initialCards
 const placesList = document.querySelector('.places__list');
-initialCards.forEach((cardContent) => {
-    const cardElement = createCard(cardContent, deleteCard, openImagePopup);
-    placesList.append(cardElement);
-});
 
 // Обработчики кнопок
 buttonEditProfile.addEventListener('click', () => {
@@ -89,9 +84,6 @@ function handleFormSubmit(evt) {
     const nameValue = nameInput.value;
     const jobValue = jobInput.value;
 
-    const profileTitle = document.querySelector('.profile__title');
-    const profileDescription = document.querySelector('.profile__description');
-
     profileTitle.textContent = nameValue;
     profileDescription.textContent = jobValue;
 
@@ -111,3 +103,17 @@ function openImagePopup(cardContent) {
 
 // Включаем валидацию всех форм
 enableValidation(validationConfig);
+
+const profileTitle = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+
+Promise.all([getUserInfo(), getInitialCards()])
+    .then(([userData, initialCards]) => {
+        initialCards.forEach((cardContent) => {
+            const cardElement = createCard(cardContent, deleteCard, openImagePopup);
+            placesList.append(cardElement);
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
